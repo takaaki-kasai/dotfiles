b74ae40480420f70710e8956a77c3fd019cc482e
Takaaki Kasai <high.river@gmail.com>
2015-05-31 20:32:52 +0900

    インストール自動化スクリプト追加

---
+++ dotfilesInstall.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+EMAIL_USER=`echo 'uvtu.evire' | tr nopqrstuvwxyzabcdefghijklm abcdefghijklmnopqrstuvwxyz`
+EMAIL_ATMARK='@'
+EMAIL_DOMAIN=`echo 'tznvy.pbz' | tr nopqrstuvwxyzabcdefghijklm abcdefghijklmnopqrstuvwxyz`
+
+cd ~/dotfiles
+git config user.name 'Takaaki Kasai'
+git config user.email "$EMAIL_USER$EMAIL_ATMARK$EMAIL_DOMAIN"
+
+cd ~
+git config --global diff.algorithm 'histogram'
+git config --global alias.l 'log --graph --all --date=short --date-order --format="%C(yellow)%h%C(reset) %C(magenta)[%ad]%C(reset)%C(auto)%d%C(reset) %s %C(cyan)@%an%C(reset)"'
+if [ `uname` = 'Darwin' ]; then
+  #mac用のコード
+  git config --global core.editor 'env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim'
+  git config --global diff.tool 'macvim'
+  git config --global difftool.macvim.cmd 'env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/vimdiff $LOCAL $REMOTE'
+elif [ `uname` = 'Linux' ]; then
+  #Linux用のコード
+  git config --global core.editor 'vim'
+  git config --global diff.tool 'vimdiff'
+fi
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
