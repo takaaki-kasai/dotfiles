349f3de61a3ede5720d91c892b38789627b390f8
Takaaki Kasai <high.river@gmail.com>
2016-07-19 23:00:48 +0900

    git diff系のコマンド体系を整理
    dd -> 通常表示, dl -> リスト表示, dt -> tool
    
    末尾が
    u -> unstaged,
    s -> staged,
    d -> カーソルが乗っているコミット内,
    h -> カーソルが乗っているコミットとHEAD間

--- dotfilesInstall.sh
+++ dotfilesInstall.sh
@@ -12,8 +12,12 @@ git config --global diff.algorithm 'histogram'
 git config --global diff.compactionHeuristic true
 git config --global core.whitespace trailing-space,tab-in-indent
 git config --global push.default nothing
-git config --global alias.d 'diff -M100%'
-git config --global alias.dt 'difftool -M100%'
+git config --global alias.ddu 'diff -M100%'
+git config --global alias.dds 'diff -M100% --cached'
+git config --global alias.dlu 'diff -M100% --name-status'
+git config --global alias.dls 'diff -M100% --name-status --cached'
+git config --global alias.dtu 'difftool -M100%'
+git config --global alias.dts 'difftool -M100% --cached'
 git config --global alias.l 'log --graph --all --date=short --date-order --format="%C(yellow)%h%C(reset) %C(magenta)[%ad]%C(reset)%C(auto)%d%C(reset) %s %C(cyan)@%an%C(reset)"'
 git config --global alias.lb 'log --graph --date=short --date-order --format="%C(yellow)%h%C(reset) %C(magenta)[%ad]%C(reset)%C(auto)%d%C(reset) %s %C(cyan)@%an%C(reset)"'
 git config --global alias.s 'status'
